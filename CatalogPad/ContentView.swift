9f0d620eb711ac1b0202fafa50870d35f6a1920d
Raphael Guye <raphael.guye@gmail.com>
2022-06-24 14:07:16 +0200

    Initial Commit

---
+++ CatalogPad/ContentView.swift
@@ -0,0 +1,23 @@
+//
+// Created by Raphael Guye on 24.06.22
+// https://github.com/raphaelguye
+// 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
