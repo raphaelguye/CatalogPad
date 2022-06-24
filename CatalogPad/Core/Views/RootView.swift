3c2ccb22134ad9d0df14fba1a3d97ab246c273d7
Raphael Guye <raphael.guye@gmail.com>
2022-06-24 15:45:27 +0200

    manage the part 'content' of the splitview

--- CatalogPad/Core/Views/RootView.swift
+++ CatalogPad/Core/Views/RootView.swift
@@ -8,11 +8,19 @@ struct RootView: View {
 
   var body: some View {
     NavigationSplitView {
-      List(menus, selection: $menuSelected) { menu in
+      List(menus, id: \.self, selection: $menuSelected) { menu in
         Label(menu.title, systemImage: menu.systemImage)
+
       }
     } content: {
-      Text("Content")
+      List {
+        switch menuSelected?.type {
+        case .products: Text("Products list")
+        case .promos: Text("Promos list")
+        case .users: Text("Users list")
+        default: Text("Choose a section in the sidebar")
+        }
+      }
     } detail: {
       Text("Detail")
     }
@@ -21,15 +29,24 @@ struct RootView: View {
   // MARK: Private
 
   private struct Menu: Identifiable, Hashable {
+
+    enum MenuType {
+      case products
+      case promos
+      case users
+    }
+
     var id: String
     var title: String
     var systemImage: String
+
+    var type: MenuType
   }
 
   @State private var menus = [
-    Menu(id: "1", title: "Products", systemImage: "cart.circle"),
-    Menu(id: "2", title: "Promos", systemImage: "dollarsign.circle"),
-    Menu(id: "3", title: "Users", systemImage: "person.crop.circle"),
+    Menu(id: "1", title: "Products", systemImage: "cart.circle", type: .products),
+    Menu(id: "2", title: "Promos", systemImage: "dollarsign.circle", type: .promos),
+    Menu(id: "3", title: "Users", systemImage: "person.crop.circle", type: .users),
   ]
 
   @State private var menuSelected: Menu?
